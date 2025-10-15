251cc7fac9911265a0d20482b9a7796d3b41b03d
Maksymilian Janica <maksymilianjanica2001@gmail.com>
2025-10-15 19:21:30 +0200

    lab02

---
+++ lab02/index.ts
@@ -0,0 +1,37 @@
+//Zad1
+// const processData = ( name: string, age: number, isActive: boolean, scores: number[], status: 'active'|'inactive' ) => {
+//     console.log("Name: " + name + ", Status: " + status);
+//     if (age >= 18) {
+//         console.log("Adult");
+//     } else {
+//         console.log("Minor");
+//     };
+//     const averageScore: number = scores.reduce((acc:number, curr: number) => {
+//         return acc+curr
+//     }, 0)/scores.length
+//     console.log(`Average Score: ${averageScore}`);
+// }
+
+// processData("marek", 18, true, [1,2,3], 'active');
+
+//Zad2
+
+interface Product {
+    id: number,
+    name: string,
+    price: number
+};
+
+interface DigitalProduct extends Product {
+    fileSizeMB: number,
+    downloadLink: string
+}
+
+interface PhysicalProduct extends Product {
+    weightKg: number,
+    dimensions: number[]
+}
+
+const getProductSummary = (item: DigitalProduct | PhysicalProduct) => {
+    
+}
