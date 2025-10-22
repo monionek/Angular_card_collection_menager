f614d75ab884816ba8a2d55a829206a8c8e5d60b
Maksymilian Janica <maksymilianjanica2001@gmail.com>
2025-10-22 19:30:46 +0200

    started lab03

---
+++ lab03/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { MainContent } from './components/main-content/main-content';
+import { Sidebar } from './components/sidebar/sidebar';
+@Component({
+  selector: 'app-root',
+  imports: [Header, MainContent, Sidebar, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('lab03');
+}
