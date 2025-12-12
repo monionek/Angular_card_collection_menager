6541815689a04677337b94764e0abb2af5d26e8f
Maksymilian Janica <maksymilianjanica2001@gmail.com>
2025-12-12 15:30:02 +0100

    TODO lab10

---
+++ lab10_2/src/app/pages/book/bookdetails/bookdetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookdetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookdetails.component.html',
+  styleUrl: './bookdetails.component.scss'
+})
+export class BookdetailsComponent {
+
+}
