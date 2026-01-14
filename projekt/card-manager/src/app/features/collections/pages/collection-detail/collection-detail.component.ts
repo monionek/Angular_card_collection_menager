c0d98d3d9ae1e3cd3c83dce0d742be020fc4bb2e
Monionek <maksymilianjanica2001@gmail.com>
2026-01-14 18:39:33 +0100

    however decided to not implement formArray in collection detail

--- projekt/card-manager/src/app/features/collections/pages/collection-detail/collection-detail.component.ts
+++ projekt/card-manager/src/app/features/collections/pages/collection-detail/collection-detail.component.ts
@@ -1,18 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CollectionsService } from '../../services/collections.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { Observable, switchMap, of, map, tap, take } from 'rxjs';
-import { Collection, Color } from '../../../models/collection.model';
+import { Observable, switchMap, of, map} from 'rxjs';
+import { Collection } from '../../../models/collection.model';
 import { AsyncPipe } from '@angular/common';
 import { NotificationService } from '../../../../core/services/notification.service';
 import { AuthService } from '../../../../core/services/auth.service';
-import { FormArray, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { colorsValidator } from '../collection-form/colorValidator';
-import { outputToObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-collection-detail',
-  imports: [AsyncPipe, RouterLink, ReactiveFormsModule],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './collection-detail.component.html',
   styleUrl: './collection-detail.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,7 +22,6 @@ export class CollectionDetailComponent {
   private readonly router: Router = inject(Router);
   public readonly isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
   private collectionId: string | null = null;
-  private readonly formBuilder: NonNullableFormBuilder = inject(NonNullableFormBuilder);
   public readonly collection$: Observable<Collection | null> =
     this.route.paramMap.pipe(
       map((paramMap) => {
@@ -44,52 +40,6 @@ export class CollectionDetailComponent {
         return this.collectionsService.getById(id);
       })
   );
-  private readonly avaiableColors$: Observable<Color[]> = this.collection$
-  .pipe(
-    (map((collection) => {
-       if (collection === null) {return [];} 
-      const colors: Color[] = [... collection.colors];
-      if (colors.some((c) => c !== "COLORLESS")) {
-        colors.push("COLORLESS");
-      }
-
-      return colors;
-    })));
-
-
-  public readonly collectionDetailForm = this.formBuilder.group({
-    cards: this.formBuilder.array([])
-  });
-
-  private createCardForm(colors: Color[]): FormGroup {
-    return this.formBuilder.group({
-      name: ['', { validators: [Validators.required, Validators.minLength(2)]}],
-      colors: this.buildColorsGroup(colors),
-      manaCost: [0, { validators: [Validators.required, Validators.min(0)]}],
-      attack: [0, { validators: [Validators.required, Validators.min(0)]}],
-      health: [1, { validators: [Validators.required, Validators.min(1)]}],
-
-    });
-  }
-
-  public get cardsFormArray(): FormArray {
-    return this.collectionDetailForm.get('cards') as FormArray;
-  }
-
-  public buildColorsGroup(colors: Color[]): FormGroup {
-    const controls: Record<Color, FormControl<boolean>> = {} as Record<
-      Color,
-      FormControl<boolean>
-    >;
-
-    for (const color of colors) {
-      controls[color] = this.formBuilder.control(false);
-    }
-
-    return this.formBuilder.group(controls, {
-      validators: colorsValidator(),
-    });
-  }
 
   public delete(): void {
     if (!this.collectionId) {
